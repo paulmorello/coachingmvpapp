4e38b448fa47732d22660aa307693a5b57926fc3
paulmorello <paulmorello1@gmail.com>
2017-07-11 21:36:09 -0700

    practice session form created

--- app/controllers/practice_sessions_controller.rb
+++ app/controllers/practice_sessions_controller.rb
@@ -6,6 +6,28 @@ class PracticeSessionsController < ApplicationController
   def new
   end
 
+  def create
+
+    # Create a new practice session
+    practice = PracticeSession.new
+    practice.user_id = current_user.id
+    practice.title = params[:practice_title]
+    practice.date = params[:practice_date]
+
+    if params[:video_file] != ""
+      practice.practice_session_url = params[:video_file]
+    else
+      practice.practice_session_url = params[:video_url]
+    end
+
+    if practice.save!
+      redirect_to '/dashboard'
+    else
+      redirect_to '/practice_sessions/new'
+    end
+    
+  end
+
   def show
   end
 
--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -18,9 +18,6 @@ class VideosController < ApplicationController
       video.video_url = params[:video_url]
     end
 
-    # Check if user uploads practice session or game review
-    if params[:upload_type] == 'practice_session'
-
       # If user selects practice session, create a new practice session
       practice_session = PracticeSession.new
       practice_session.user_id = current_user.id
@@ -38,32 +35,6 @@ class VideosController < ApplicationController
         redirect_to 'videos/new'
       end
 
-    elsif params[:upload_type] == 'game_review'
-
-      # create new game review
-      game = Game.new
-      game.user_id = current_user.id
-      game.title = params[:title]
-      game.date = params[:game_date]
-
-      if params[:video_file] != ""
-        game.game_url = params[:video_file]
-      else
-        game.game_url = params[:video_url]
-      end
-
-      game.team_name = params[:team_name]
-      game.opponent_name = params[:opponent_name]
-      game.team_score = params[:team_score]
-      game.opponent_score = params[:opponent_score]
-      game.player_number = params[:player_number]
-      game.needs_review = true
-
-      if game.save!
-        video.game_id = game.id
-      else
-        redirect_to 'videos/new'
-      end
     end
 
     @errors = {}
