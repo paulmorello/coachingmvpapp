b5b64691a76d4413a04f3dcf00237e00d29d555a
paulmorello <paulmorello1@gmail.com>
2017-08-24 21:34:27 -0700

    fixed password retrieval feature

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,5 +1,12 @@
 class PasswordResetsController < ApplicationController
+
   def new
+    if !logged_in
+      redirect_to_route_if_not_logged_in(route = '/login')
+    else
+      redirect_to_route_if_logged_in(route = "/dashboard/#{current_user.username}")
+    end
+
   end
 
   def create
@@ -9,23 +16,29 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit
+    if !logged_in
+      redirect_to_route_if_not_logged_in(route = '/login')
+    else
+      redirect_to_route_if_logged_in(route = "/dashboard/#{current_user.username}")
+    end
+    
     @user = User.find_by_password_reset_token!(params[:id])
   end
 
   def update
-    @user = User.find_by_password_reset_token!(params[:id])
 
+    @user = User.find_by_password_reset_token!(params[:id])
     if @user.password_reset_sent_at < 2.hours.ago
 
       redirect_to new_password_reset_path, :alert => "Password reset has expired."
 
     elsif params["user"]["password"] == params["user"]["password"]
-
       @user.password = params["user"]["password"]
 
       if @user.save
         redirect_to '/', :notice => "Password has been reset!"
       end
+
     else
       render :edit
     end
