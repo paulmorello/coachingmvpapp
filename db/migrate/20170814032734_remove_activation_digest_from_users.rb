bfc1f1f485baf51438c77e1ece1efe9c057cf536
paulmorello <paulmorello1@gmail.com>
2017-08-29 21:42:00 -0700

    updated schema

--- db/migrate/20170814032734_remove_activation_digest_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveActivationDigestFromUsers < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :users, :activation_digest, :string
-  end
-end
--- db/migrate/20170814032845_remove_activated_at_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveActivatedAtFromUsers < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :users, :activated_at, :datetime
-  end
-end
